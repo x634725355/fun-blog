ff93f9b0661a414b21d97291d52af75fd523727d
rider <rider@put.com>
2025-10-17 16:00:40 +0800

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@pinia/nuxt', '@nuxt/ui', '@vueuse/nuxt', "nitro-cloudflare-dev"],
+  modules: ['@pinia/nuxt', '@nuxt/ui', '@vueuse/nuxt', 'nitro-cloudflare-dev'],
   devtools: { enabled: true },
 
   devServer: {
@@ -21,6 +21,14 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       APP_ENV: process.env.NUXT_APP_ENV || 'dev',
+    },
+  },
+  nitro: {
+    preset: 'cloudflare_module',
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
     },
   },
   sourcemap: false,
